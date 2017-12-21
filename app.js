1cb1bcec331a4891f2548fec6637fe347a9bd6c5
Antonio Valdez Arce <a.valdez.arce@gmail.com>
2017-12-21 17:12:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ app.get('/', function (req, res) { res.status(200).send('Woof Woof!'); });
 app.get('/listen', function (req, res) { res.status(200).send('Woof Woof!'); });
 
 app.post('/listen', function (req, res, next) {
+  
+  var reqType = req.body.type;
   var userName = req.body.user_name;
+  
+  if (reqType === 'url_verification') {
+    var challenge = {challenge: req.body.challenge}
+    return res.status(200).json(challenge);
+  }
   
   var botPayload = {
     text: 'Woof Woof Woof!'
