1e83c85ea40a8dc5efd5d6f3e4f80bf7184d3de0
Antonio Valdez Arce <antonio.valdez@kiwee.eu>
2017-12-21 19:59:22 +0100

    Enabled request

--- app.js
+++ app.js
@@ -26,25 +26,25 @@ app.post('/listen', function (req, res, next) {
 
   if (userName !== 'slackbot' && userName !== 'slack' && userName !== 'maluch' ) {
 
-    // var requestData = { "text": "Woof woof woof!" };
-    // var url = 'https://hooks.slack.com/services/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';
+    var requestData = { "text": "Woof woof woof!" };
+    var url = 'https://hooks.slack.com/services/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';
   
-    // request({
-    //   url: url,
-    //   json: requestData,
-    //   method: "POST",
-    //   }, function (error, response, body) {
-    //       if (!error && response.statusCode === 200) {
-    //           console.log(body)
-    //           return res.status(200).end();
-    //       }
-    //       else {
-    //           console.log("error: " + error)
-    //           console.log("response.statusCode: " + response.statusCode)
-    //           console.log("response.statusText: " + response.statusText)
-    //           return res.status(500).end();
-    //       }
-    //   });
+    request({
+      url: url,
+      json: requestData,
+      method: "POST",
+      }, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+              console.log(body)
+              return res.status(200).end();
+          }
+          else {
+              console.log("error: " + error)
+              console.log("response.statusCode: " + response.statusCode)
+              console.log("response.statusText: " + response.statusText)
+              return res.status(500).end();
+          }
+      });
   
   } else {
     return res.status(200).end();
