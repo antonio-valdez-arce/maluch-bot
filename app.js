2cabf5471db24d9271efd97edf995f3e2bcca923
Antonio Valdez Arce <a.valdez.arce@gmail.com>
2017-12-21 17:45:28 +0100

    Added post req

--- app.js
+++ app.js
@@ -21,12 +21,27 @@ app.post('/listen', function (req, res, next) {
     return res.status(200).json(challenge);
   }
   
-  var botPayload = {
-    text: 'Woof Woof Woof!'
+  var post_data = querystring.stringify({
+      'text' : 'Woof woof woof!'
+  });
+  
+  var post_options = {
+      host: 'hooks.slack.com',
+      port: '80',
+      path: '/services/T3ZEF9U2D/B8HD54588/ehLb3IUvhH8OgbrOFn7Y8quS',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
   };
 
   if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
+    var post_req = http.request(post_options);
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
   } else {
     return res.status(200).end();
   }
